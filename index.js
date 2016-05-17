833efa6992814b734ea4293cc21948b4472e512c
Benjy Cui <benjytrys@gmail.com>
2016-05-17 09:47:40 +0800

    feat: import all the languages

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const prismCore = 'prismjs/components/prism-core';
+const Prism = require(prismCore);
+
+const prismComponents = path.dirname(require.resolve(prismCore));
+const prelude = [
+  'prism-clike', 'prism-javascript', 'prism-markup',
+  'prism-c', 'prism-ruby', 'prism-css',
+];
+
+function uniq(arr) {
+  const set = arr.reduce((set, item) => {
+    set[item] = true;
+    return set;
+  }, {});
+  return Object.keys(set);
+}
+
+const components = prelude.concat(fs.readdirSync(prismComponents))
+        .map((component) => component.replace(/(\.min)?\.js$/, ''));
+const componentsSet = uniq(components);
+componentsSet
+  .forEach((component) => require(path.join(prismComponents, component)));
+
+module.exports = Prism;
